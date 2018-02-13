891fd2128629e507c928b14d006ef3d180c4e53f
roman <roman.melnishin@gmail.com>
2018-02-13 13:37:32 +0200

    edit form in progress

--- EditForm.spec.js
+++ EditForm.spec.js
@@ -3,8 +3,13 @@ import i18n from '@/translations'
 import { shallow, mount } from '@vue/test-utils'
 import Vuex from 'vuex'
 import EditForm from '@/components/edit-form/EditForm'
+import userMock from '../../fixtures/userMock.json'
 import productItemMock from '../../fixtures/productItemMock.json'
+import productTypesMock from '../../fixtures/productTypesMock.json'
 import productPlatformsMock from '../../fixtures/productPlatformsMock.json'
+import productServicesMock from '../../fixtures/productServicesMock.json'
+import productGamesMock from '../../fixtures/productGamesMock.json'
+import productGameSeriesMock from '../../fixtures/productGameSeriesMock.json'
 
 const { messages } = i18n
 const propsData = {
@@ -25,15 +30,15 @@ describe('EditForm.spec.js', () => {
     store = new Vuex.Store({
       state: {},
       actions: {
-        loadProductTypes: jest.fn(),
-        loadGameSeries: jest.fn()
+        loadProductTypes: jest.fn(() => productTypesMock),
+        loadGameSeries: jest.fn(() => productGameSeriesMock)
       },
       getters: {
         getProductTypes: jest.fn(() => []),
-        getUser: jest.fn(),
+        getUser: jest.fn(() => userMock),
         getPlatformList: jest.fn(() => productPlatformsMock),
-        getServiceList: jest.fn(),
-        getGameList: jest.fn()
+        getServiceList: jest.fn(() => productServicesMock),
+        getGameList: jest.fn(() => productGamesMock)
       }
     })
 
@@ -112,7 +117,46 @@ describe('EditForm.spec.js', () => {
 
   it('Render multiselect with correct options', () => {
     const multiselectOptions = wrapper.findAll('.multiselect__element')
-    expect(multiselectOptions.length).toBe(productPlatformsMock.length)
-    expect(multiselectOptions.wrappers[0].text()).toBe(productPlatformsMock[0].name)
+    expect(multiselectOptions.length).toBe(store.getters.getPlatformList.length)
+    expect(multiselectOptions.at(0).text()).toBe(store.getters.getPlatformList[0].name)
+  })
+
+  it('Render multiservices selects', () => {
+    const servicesContainer = wrapper.find('.added-service')
+    const selects = servicesContainer.findAll('select')
+    expect(servicesContainer.exists()).toBe(true)
+    expect(selects.length).toBe(3)
+
+    const serviceSelect = selects.at(0)
+    const gameSelect = selects.at(1)
+    const gameSeriesSelect = selects.at(2)
+    const { multiservice } = propsData.preloaded
+
+    // console.log(multiservice[0].service.name)
+    expect(serviceSelect.find(':selected').element._value.name).toBe(multiservice[0].service.name)
+    expect(gameSelect.find(':selected').element._value.name).toBe(multiservice[0].game.name)
+    // console.log(wrapper.vm.selectedServices.selectedSeries)
+    // console.log(wrapper.html())
+
+    // expect(wrapper.vm.selectedServices.selectedSeries).toEqual(productGameSeriesMock)
+    // wrapper.update()
+    // wrapper.vm.selectedServices.selectedSeries = productGameSeriesMock
+    console.log('---')
+    console.log(wrapper.vm.selectedServices)
+    const selectedServices = { ...wrapper.vm.selectedServices[0], selectedSeries: productGameSeriesMock }
+    wrapper.setData({ selectedServices: [selectedServices] })
+    console.log(wrapper.vm.selectedServices)
+    console.log(wrapper.html())
+    // console.log(wrapper.html())
+    // console.log(multiservice[0].series.name)
+    // expect(gameSeriesSelect.find(':selected').element._value.name).toBe(multiservice[0].series.name)
+
+    // expect(serviceSelect.findAll('option').length).toBe(multiservice[0].service.name)
+    // expect(gameSelect.findAll('option').length).toBe(store.getters.getGameList.length + 1)
+    // expect(gameSeriesSelect.findAll('option').length).toBe(store.getters.getGameList.length + 1)
+
+    // expect(serviceSelect.findAll('option').length).toBe(store.getters.getServiceList.length + 1)
+    // expect(gameSelect.findAll('option').length).toBe(store.getters.getGameList.length + 1)
+    // expect(gameSeriesSelect.findAll('option').length).toBe(store.getters.getGameList.length + 1)
   })
 })
